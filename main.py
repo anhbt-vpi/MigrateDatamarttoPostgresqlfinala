557971c62d1fb9e46da4b38d83eee9605247dac5
Anhbt <anhbt@vpi.pvn.vn>
2023-03-30 08:41:57 +0700

    Just update data

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine, Table, MetaData, text, Column, Integer, String
-
-
+from sqlalchemy.schema import PrimaryKeyConstraint, ForeignKeyConstraint
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy import ForeignKey
 def migrateDB():
 
-    server = 'xznozrobo3funm76yoyaoh75wm-lvvgvquleiuurnfvyvnetw7hoq.datamart.pbidedicated.windows.net'
-    database = 'Oil price forecast'
+    server = 'xznozrobo3funm76yoyaoh75wm-fr3e3p3dk6eejffi7w4p27iybe.datamart.pbidedicated.windows.net'
+    database = '2023_LPG_Datamart_Hanhdh'
     username = 'api@oilgas.ai'
     password = 'Vpi167YmWwnLEgac'
     driver = '{ODBC Driver 18 for SQL Server}'
@@ -22,28 +23,34 @@ def migrateDB():
     # Tạo engine để kết nối đến cơ sở dữ liệu postgresql trên local
     engine_postgresql = create_engine(f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
 
-    metadata = MetaData()
-    table_from_another_database = Table('fact_oilprice_input', metadata, autoload_with=engine_datamart)
-
-    # Tạo bảng tương tự trong engine_postgresql
-    table_in_postgresql = Table('fact_oilprice_input', metadata,
-                                *[Column(c.name,  String if 'NVARCHAR' in str(c.type) else c.type, nullable=c.nullable) for c in
-                                  table_from_another_database.columns], extend_existing=True)
-
-    metadata.create_all(engine_postgresql)
+    metadata_datamart = MetaData()
+    table_from_datamart = Table('Date_table', metadata_datamart, autoload_with=engine_datamart)
+    metadata_postgresql = MetaData()
+    table_in_postgresql = Table('Date_table', metadata_postgresql,
+                      autoload_with=engine_postgresql)
 
 
     with engine_datamart.connect() as conn_datamart, engine_postgresql.connect() as conn_postgresql:
-        select_query = table_from_another_database.select()
-        data = [dict(zip(table_from_another_database.columns.keys(),
+        select_query = table_from_datamart.select().limit(111)
+        data = [dict(zip(table_from_datamart.columns.keys(),
                          [str(val) if isinstance(val, bytes) else val for val in row])) for row in
                 conn_datamart.execute(select_query).fetchall()]
 
-        insert_query = table_in_postgresql.insert()
-        conn_postgresql.execute(insert_query, data)
+        # insert_query = table_in_postgresql.insert()
+        # conn_postgresql.execute(insert_query, data)
+        insert_query = insert(table_in_postgresql).values(data).on_conflict_do_nothing()
+        conn_postgresql.execute(insert_query)
         conn_postgresql.commit()
 
-
+    # # Định nghĩa cột primary key
+    # primary_key_columns = [c for c in table_in_postgresql.columns if c.primary_key]
+    # insert_stmt = insert(table_in_postgresql).values(data)
+    #
+    # # Chỉ định bỏ qua các giá trị trùng lặp
+    # on_conflict_stmt = insert_stmt.on_conflict_do_nothing(index_elements=primary_key_columns)
+    # with engine_postgresql.connect() as conn_postgresql:
+    #     conn_postgresql.execute(on_conflict_stmt)
+    #     conn_postgresql.commit()
 if __name__ == '__main__':
     migrateDB()
 

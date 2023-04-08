038ec4281552dd422099445b7110ebeb63f09a32
Anhbt <anhbt@vpi.pvn.vn>
2023-04-08 12:29:44 +0700

    Update loop table

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, Table, MetaData, text, Column, Integer, String
-from sqlalchemy.schema import PrimaryKeyConstraint, ForeignKeyConstraint
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.dialects.postgresql import insert
-from sqlalchemy import ForeignKey
+from sqlalchemy.dialects.postgresql import insert as postgresql_insert
 def migrateDB():
 
     server = 'xznozrobo3funm76yoyaoh75wm-fr3e3p3dk6eejffi7w4p27iybe.datamart.pbidedicated.windows.net'
@@ -10,7 +10,6 @@ def migrateDB():
     password = 'Vpi167YmWwnLEgac'
     driver = '{ODBC Driver 18 for SQL Server}'
     params = 'Driver=' + driver + ';Server=' + server + ',1433;Database=' + database + ';Uid={' + username + '};Pwd={' + password + '};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;Authentication=ActiveDirectoryPassword'
-
 
     db_name = 'test'
     db_user = 'postgres'
@@ -23,34 +22,53 @@ def migrateDB():
     # Tạo engine để kết nối đến cơ sở dữ liệu postgresql trên local
     engine_postgresql = create_engine(f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
 
-    metadata_datamart = MetaData()
-    table_from_datamart = Table('Date_table', metadata_datamart, autoload_with=engine_datamart)
-    metadata_postgresql = MetaData()
-    table_in_postgresql = Table('Date_table', metadata_postgresql,
-                      autoload_with=engine_postgresql)
+    Session = sessionmaker(autocommit=False, autoflush=False, bind=engine_datamart)
+    db = Session()
+    #Lấy các table trong db datamart
+    str_sql = text("SELECT table_name FROM information_schema.tables")
+    result_query = db.execute(str_sql)
+    results = result_query.fetchall()
+    table_names = [row[0] for row in results]
+    drop = ["relationshipColumns", "relationships", "database_firewall_rules"]
+    list_table = [elem for elem in table_names if elem not in drop]
+    #Lặp qua các table và ghi dữ liệu vao db postgres
+    with engine_datamart.connect().execution_options(timeout=600) as conn_datamart, engine_postgresql.connect().execution_options(timeout=600) as conn_postgresql:
+        for tableName in list_table:
+            writeData(engine_postgresql, engine_datamart, conn_datamart, conn_postgresql , tableName)
+        conn_postgresql.close()
+        conn_datamart.close()
 
 
-    with engine_datamart.connect() as conn_datamart, engine_postgresql.connect() as conn_postgresql:
-        select_query = table_from_datamart.select().limit(111)
-        data = [dict(zip(table_from_datamart.columns.keys(),
-                         [str(val) if isinstance(val, bytes) else val for val in row])) for row in
-                conn_datamart.execute(select_query).fetchall()]
+def writeData(engine_postgresql, engine_datamart, conn_datamart, conn_postgresql, tablename):
+    print(tablename)
+    metadata = MetaData()
+    table_from_another_database = Table(tablename, metadata, autoload_with=engine_datamart)
 
-        # insert_query = table_in_postgresql.insert()
-        # conn_postgresql.execute(insert_query, data)
-        insert_query = insert(table_in_postgresql).values(data).on_conflict_do_nothing()
-        conn_postgresql.execute(insert_query)
+    # Tạo bảng tương tự trong engine_postgresql
+
+    table_in_postgresql = Table(tablename, metadata,
+                                *[Column(c.name,  String if 'NVARCHAR' in str(c.type) else c.type, nullable=c.nullable) for c in
+                                  table_from_another_database.columns], extend_existing=True)
+
+    metadata.create_all(engine_postgresql)
+
+    select_query = table_from_another_database.select()
+    data = [dict(zip(table_from_another_database.columns.keys(),
+                     [str(val) if isinstance(val, bytes) else val for val in row])) for row in
+            conn_datamart.execute(select_query).fetchall()]
+
+    # Đặt kích thước lô
+    batch_size = 500
+
+    # Lặp lại cho đến khi không còn dữ liệu để ghi
+
+    while data:
+        batch = data[:batch_size]
+        data = data[batch_size:]
+        insert_query = postgresql_insert(table_in_postgresql).values(batch).on_conflict_do_nothing()
+        conn_postgresql.execute(insert_query, batch)
         conn_postgresql.commit()
 
-    # # Định nghĩa cột primary key
-    # primary_key_columns = [c for c in table_in_postgresql.columns if c.primary_key]
-    # insert_stmt = insert(table_in_postgresql).values(data)
-    #
-    # # Chỉ định bỏ qua các giá trị trùng lặp
-    # on_conflict_stmt = insert_stmt.on_conflict_do_nothing(index_elements=primary_key_columns)
-    # with engine_postgresql.connect() as conn_postgresql:
-    #     conn_postgresql.execute(on_conflict_stmt)
-    #     conn_postgresql.commit()
 if __name__ == '__main__':
     migrateDB()
 
